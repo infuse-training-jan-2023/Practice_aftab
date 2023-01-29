caecd857aacdde953a25304a9ff1140d3ee55f59
aftab <aftab.saeedi2015@gmail.com>
2023-01-29 17:42:08 +0530

    added dockerfile

--- modules/web_framework.rb
+++ modules/web_framework.rb
@@ -4,8 +4,13 @@ require 'selenium-webdriver'
 class Web_framework
   @url = nil
   def initialize()
-    Selenium::WebDriver::Chrome::Service.driver_path="modules\\chromedriver.exe"
-    @driver = Selenium::WebDriver.for :chrome
+    Selenium::WebDriver::Chrome::Service.driver_path=ENV['driver']
+    options = Selenium::WebDriver::Chrome::Options.new
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--window-size=1920,1080')
+        @driver = Selenium::WebDriver.for :chrome ,options: options
   end
   def get_webpage(url)
     @url = url
--- testing/testing.rb
+++ testing/testing.rb
@@ -1,57 +1,49 @@
 require "test/unit"
-require_relative 'C:\Users\Aftab\Desktop\webTestFramework\modules\web_framework.rb'
+require './modules/web_framework.rb'
 
 
 class Testing < Test::Unit::TestCase
 
   def test_dropdown_options
     driver = Web_framework.new
-    url = 'http://127.0.0.1:5500/form/form.html'
+    url = 'file:///form/form.html'
     driver.get_webpage(url)
     dropdown = driver.find_element({:id=>'language'})
     assert_equal(true,driver.check_dropdown_options(dropdown,"english"))
   end
   def test_dropdown_item_selection
     driver = Web_framework.new
-    driver.get_webpage('http://127.0.0.1:5500/form/form.html')
+    driver.get_webpage('file:///form/form.html')
     dropdown = driver.find_element({:id=>'language'})
     assert_equal('English',driver.select_item_from_dopdown(dropdown,1).text)
   end
   def test_find_element
     driver = Web_framework.new
-    driver.get_webpage('http://127.0.0.1:5500/form/form.html')
+    driver.get_webpage('file:///form/form.html')
     assert_not_equal(nil,driver.find_element({:id=>'language'}))
   end
   def test_find_elements
     driver = Web_framework.new
-    driver.get_webpage('http://127.0.0.1:5500/form/form.html')
+    driver.get_webpage('file:///form/form.html')
     assert_not_equal(nil,driver.find_elements({:tag_name=>'options'}))
   end
   def test_element_click
     driver = Web_framework.new
-    driver.get_webpage('http://127.0.0.1:5500/form/form.html')
+    driver.get_webpage('file:///form/form.html')
     # wrong id for element to check for negative case
     element = driver.find_element({:id=>'languages'})
     assert_equal("null element",driver.click_element(element))
   end
   def test_page_title
     driver = Web_framework.new
-    driver.get_webpage('http://127.0.0.1:5500/form/form.html')
+    driver.get_webpage('file:///form/form.html')
     assert_equal("form",driver.get_page_title())
   end
   def test_wait
     driver = Web_framework.new
-    driver.get_webpage('http://127.0.0.1:5500/form/form.html')
+    driver.get_webpage('file:///form/form.html')
     # for anything except integer it returns enter a number
     assert_equal("enter a number",driver.wait("hello"))
-  end
-  def test_maximize_window
-    driver = Web_framework.new
-    driver.get_webpage('http://127.0.0.1:5500/form/form.html')
-    height = '1048'
-    driver.maximize_window
-    result = driver.get_window_size.to_s.include? height
-    assert(result)
   end
 end
 
