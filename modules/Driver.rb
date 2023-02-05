c10efd80b4c986453a57def300c013b4de9485b3
aftab <aftab.saeedi2015@gmail.com>
2023-02-05 15:28:08 +0530

    refactoring done

--- modules/Driver.rb
+++ modules/Driver.rb
@@ -5,10 +5,10 @@ class Driver
   def get_driver_with_options()
         Selenium::WebDriver::Chrome::Service.driver_path=ENV['driver']
         options = Selenium::WebDriver::Chrome::Options.new
-            # options.add_argument('--headless')
-            # options.add_argument('--no-sandbox')
-            # options.add_argument('--disable-dev-shm-usage')
-            # options.add_argument('--window-size=1920,1080')
+            options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            options.add_argument('--window-size=1920,1080')
         return Selenium::WebDriver.for :chrome ,options: options
   end
 end
